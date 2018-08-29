e5c497f12ccba3dd62a3313c9da36b8eb202f571
unknown <jussi.kaatiala@gmail.com>
2018-08-29 12:23:03 +0300

    Added option to copy file instead of moving

--- icllibm/collectFiles.m
+++ icllibm/collectFiles.m
@@ -1,16 +1,17 @@
-function collectFiles(parentdir, destinationdir, pattern)
-    %Function collectFiles(parentdir, destinationdir, pattern)
+function collectFiles(sourcedir, destinationdir, pattern, rmsource)
+    %Function collectFiles(sourcedir, destinationdir, pattern, rmsource)
     %
-    % Collects files from all the directories below parentdir. Files
+    % Collects files from all the directories below sourcedir. Files
     % containing pattern in the filename are transferred to destinationdir.
+    % If rmsource is 1, move file instead of copying.
     
     % TODO: option to delete empty folders
     
-    files = dir(parentdir);
+    files = dir(sourcedir);
     
     for i=1:length(files)
         file = files(i);
-        filepath = strcat(parentdir, filesep, file.name);
+        filepath = strcat(sourcedir, filesep, file.name);
         if file.isdir
             if ~(strcmp(file.name,'.') || strcmp(file.name,'..'))
                 % valid folder
@@ -20,8 +21,13 @@ function collectFiles(parentdir, destinationdir, pattern)
             % valid non-folder file
             regextest = regexp(file.name, pattern, 'ONCE');
             if ~isempty(regextest)
-                disp(strcat("Moving file:", filepath, " to ", destinationdir));
-                status = movefile(filepath, destinationdir);
+                if rmsource
+                    disp(strcat("Moving file:", filepath, " to ", destinationdir));
+                    status = movefile(filepath, destinationdir);
+                else:
+                    disp(strcat("Copying file:", filepath, " to ", destinationdir));
+                    status = copyfile(filepath, destinationdir);
+                end
                 if status == 0
                     disp("Cannot move file.");
                 end
